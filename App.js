9eb5d16066983a9ca8401cf061d51c80a4c542b5
Eric Feinstein <erichfeinstein@gmail.com>
2019-01-17 22:24:59 -0500

    players can capture areas at their location

--- App.js
+++ App.js
@@ -4,16 +4,16 @@ import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import MapView, { Circle } from 'react-native-maps';
 import axios from 'axios';
 
-import Login from './Login';
+// import Login from './Login';
 
 //FSA IP, need to change to dev location
-const IP = 'http://172.16.21.34:3000';
+const IP = 'http://192.168.1.55:3000';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: null,
+      user: { id: 1, username: 'eric', name: 'Eric', teamId: 1 },
       latitude: 0,
       longitude: 0,
       caps: [],
@@ -24,21 +24,21 @@ export default class App extends React.Component {
     this.updateState = this.updateState.bind(this);
     this.captureArea = this.captureArea.bind(this);
     this.gotCapInfoFromServer = this.gotCapInfoFromServer.bind(this);
-    this.login = this.login.bind(this);
+    // this.login = this.login.bind(this);
+    this.subscribe = this.subscribe;
   }
 
-  async login(username, password) {
-    try {
-      console.log('attempting to log in');
-      const user = await axios.post(`${IP}/login`, { username, password });
-      // console.log('logged in', user);
-      // this.setState({
-      //   user,
-      // });
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  // async login(username, password) {
+  //   try {
+  //     console.log('attempting to log in');
+  //     const user = await axios.post(`${IP}/login`, { username, password });
+  //     this.setState({
+  //       user: user.data,
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
 
   updateState(coords) {
     const latitude = coords.coords.latitude;
@@ -54,71 +54,66 @@ export default class App extends React.Component {
       caps,
     });
   }
-  gotNewCapFromServer(cap) {
-    let caps = this.state.caps;
-    caps.push(cap);
-    this.setState({
-      caps,
-    });
-  }
+
   componentDidMount() {
     this.socket.on('all-captures', this.gotCapInfoFromServer);
-    this.socket.on('new-cap');
-
+    this.socket.on('new-cap', cap => {
+      let caps = this.state.caps;
+      caps.push(cap);
+      this.setState({
+        caps,
+      });
+    });
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(this.updateState);
   }
 
-  captureArea() {
+  async captureArea() {
     console.log('emitting capture');
-    this.socket.emit('capture', {
+    await this.socket.emit('capture', {
       latitude: this.state.latitude,
       longitude: this.state.longitude,
+      teamId: this.state.user.teamId,
     });
   }
 
   render() {
+    // console.log(this.state.user);
     const latitude = this.state.latitude;
     const longitude = this.state.longitude;
     return (
       <View style={StyleSheet.absoluteFillObject}>
-        {this.state.user && this.state.user.id ? (
-          <View style={StyleSheet.absoluteFillObject}>
-            <MapView
-              style={{ ...StyleSheet.absoluteFillObject, flex: 1 }}
-              region={{
-                latitude,
-                longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }}
-              showsUserLocation={true}
-            >
-              {this.state.caps.map(cap => {
-                return (
-                  <Circle
-                    key={cap.id}
-                    strokeWidth={0}
-                    fillColor={cap.team.color}
-                    radius={cap.radius}
-                    center={{
-                      latitude: cap.latitude,
-                      longitude: cap.longitude,
-                    }}
-                  />
-                );
-              })}
-            </MapView>
+        <MapView
+          style={{ ...StyleSheet.absoluteFillObject, flex: 1 }}
+          region={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+        >
+          {this.state.caps.map(cap => {
+            return (
+              <Circle
+                key={cap.id}
+                strokeWidth={1}
+                fillColor={cap.team.color}
+                radius={cap.radius}
+                center={{
+                  latitude: cap.latitude,
+                  longitude: cap.longitude,
+                }}
+              />
+            );
+          })}
+        </MapView>
 
-            <View style={styles.footer}>
-              <View style={styles.button}>
-                <Button title="Capture" onPress={this.captureArea} />
-              </View>
-            </View>
+        <View style={styles.footer}>
+          <View style={styles.button}>
+            <Button title="Capture" onPress={() => this.captureArea()} />
           </View>
-        ) : (
-          <Login login={this.login} />
-        )}
+        </View>
       </View>
     );
   }
@@ -133,7 +128,7 @@ const styles = StyleSheet.create({
   },
   footer: {
     position: 'absolute',
-    top: Dimensions.get('window').height - 50,
+    top: Dimensions.get('window').height - 150,
     width: Dimensions.get('window').width,
   },
   button: {
