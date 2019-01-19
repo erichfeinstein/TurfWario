53986f1d221b107a4bca6acf45a3698c9c4372e1
Eric Feinstein <erichfeinstein@gmail.com>
2019-01-19 11:57:49 -0500

    lots of updates and features

--- Login.js
+++ Login.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Button, Icon } from 'react-native-elements';
+import axios from 'axios';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+
+const IP = 'http://192.168.1.55:3000';
+const sBarHeight = getStatusBarHeight();
+
 import {
+  Alert,
   StyleSheet,
   View,
+  KeyboardAvoidingView,
   Text,
   Container,
   Label,
   TextInput,
-  Button,
+  Picker,
+  Keyboard,
 } from 'react-native';
 
 export default class Login extends React.Component {
@@ -15,51 +25,166 @@ export default class Login extends React.Component {
     this.state = {
       username: '',
       password: '',
+      teamId: 1,
+      teams: [],
+      signingUp: false,
     };
+    this.login = this.login.bind(this);
   }
+
+  async componentDidMount() {
+    const teams = await axios.get(`${IP}/teams`);
+    this.setState({ teams: teams.data });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Login</Text>
-        <Text>Username</Text>
-        <TextInput
-          onChangeText={username =>
-            this.setState({
-              username,
-            })
-          }
-          style={styles.textInput}
-        />
-        <Text>Password</Text>
-        <TextInput
-          onChangeText={password =>
-            this.setState({
-              password,
-            })
-          }
-          secureTextEntry={true}
-          style={styles.textInput}
-        />
-        <Button
-          title="Log In"
-          onPress={() =>
-            this.props.login(this.state.username, this.state.password)
-          }
-        />
-      </View>
+      <KeyboardAvoidingView behavior="padding">
+        {/* Menu Button */}
+        <View style={{ top: sBarHeight + 20, left: 20, width: 40, height: 40 }}>
+          <Icon
+            size={40}
+            name="menu"
+            color="#000000"
+            onPress={() => {
+              Keyboard.dismiss();
+              this.props.navigation.openDrawer();
+            }}
+          />
+        </View>
+
+        {/* Form */}
+        <View style={styles.container}>
+          <TextInput
+            onChangeText={password => {
+              this.setState({
+                password,
+              });
+            }}
+          />
+          <Text>Username</Text>
+          <TextInput
+            value={this.state.username}
+            onChangeText={username =>
+              this.setState({
+                username,
+              })
+            }
+            style={styles.textInput}
+          />
+          <Text>Password</Text>
+          <TextInput
+            value={this.state.password}
+            onChangeText={password =>
+              this.setState({
+                password,
+              })
+            }
+            secureTextEntry={true}
+            style={styles.textInput}
+          />
+          {this.state.teams && this.state.signingUp ? (
+            <View>
+              <Text>Team</Text>
+              <Picker
+                style={{ height: 50, width: 100 }}
+                selectedValue={this.state.teamId}
+                onValueChange={teamId => this.setState({ teamId })}
+              >
+                {this.state.teams.map((team, key) => (
+                  <Picker.Item key={key} value={team.id} label={team.name} />
+                ))}
+              </Picker>
+            </View>
+          ) : (
+            undefined
+          )}
+          <Button
+            title={this.state.signingUp ? 'Sign Up' : 'Log In'}
+            buttonStyle={{
+              backgroundColor: '#3D81CA',
+              borderRadius: 10,
+              margin: 20,
+            }}
+            onPress={
+              this.state.signingUp
+                ? () =>
+                    this.signUp(
+                      this.state.username,
+                      this.state.password,
+                      this.state.teamId
+                    )
+                : () => this.login(this.state.username, this.state.password)
+            }
+          />
+          {!this.state.signingUp ? (
+            <Button
+              title="Signup Form"
+              onPress={() => this.setState({ signingUp: true })}
+            />
+          ) : (
+            <Button
+              title="Login Form"
+              onPress={() => this.setState({ signingUp: false })}
+            />
+          )}
+        </View>
+      </KeyboardAvoidingView>
     );
+  }
+
+  async login(username, password) {
+    try {
+      console.log('attempting to log in');
+      const user = await axios.post(`${IP}/login`, { username, password });
+      this.setState({
+        username: '',
+        password: '',
+      });
+      this.props.navigation.navigate('World', { user: user.data });
+    } catch (err) {
+      Alert.alert('Oops', 'There was a problem signing in', [
+        {
+          text: 'Ok',
+          style: 'cancel',
+        },
+      ]);
+    }
+  }
+  async signUp(username, password, teamId) {
+    try {
+      const user = await axios.post(`${IP}/signup`, {
+        username,
+        password,
+        teamId,
+      });
+      this.setState({
+        username: '',
+        password: '',
+      });
+      this.props.navigation.navigate('World', { user: user.data });
+    } catch (err) {
+      Alert.alert('Oops', 'There was a problem creating your account', [
+        {
+          text: 'Ok',
+          style: 'cancel',
+        },
+      ]);
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    top: 200,
+    backgroundColor: '#00000030',
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 20,
+    margin: 20,
+    marginTop: sBarHeight + 40,
   },
   textInput: {
-    backgroundColor: '#aaa',
+    margin: 10,
+    backgroundColor: '#FFFFFF',
     width: 250,
     fontSize: 30,
   },
---
+++ World.js
@@ -0,0 +1,212 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+} from 'react-native';
+import { Button, Icon } from 'react-native-elements';
+import SocketIOClient from 'socket.io-client';
+import MapView, { Circle } from 'react-native-maps';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+
+//FSA IP, need to change to dev location
+const IP = 'http://192.168.1.55:3000';
+//Heroku IP
+// const IP = 'https://turfwar-io.herokuapp.com';
+
+const sBarHeight = getStatusBarHeight();
+
+export default class World extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      caps: {},
+      playerRadius: 0, //Radius that player sees of their effective area
+    };
+    //SOCKET
+    this.socket = SocketIOClient(IP);
+
+    //Binds
+    this.updateState = this.updateState.bind(this);
+    this.captureArea = this.captureArea.bind(this);
+    this.gotCapInfoFromServer = this.gotCapInfoFromServer.bind(this);
+  }
+
+  updateState(coords) {
+    const latitude = coords.coords.latitude;
+    const longitude = coords.coords.longitude;
+    this.setState({
+      latitude,
+      longitude,
+    });
+  }
+
+  gotCapInfoFromServer(caps, radius) {
+    let mapOfCaps = {};
+    caps.map(cap => {
+      mapOfCaps[cap.id] = cap;
+    });
+    this.setState({
+      caps: mapOfCaps,
+      playerRadius: radius,
+    });
+  }
+
+  componentDidMount() {
+    this.socket.on('all-captures', this.gotCapInfoFromServer);
+    this.socket.on('new-cap', cap => {
+      let caps = this.state.caps;
+      caps[cap.id] = cap;
+      this.setState({
+        caps,
+      });
+    });
+    this.socket.on('destroy-cap', capToDestroy => {
+      let caps = this.state.caps;
+      delete caps[capToDestroy.id];
+      this.setState({
+        caps,
+      });
+    });
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(this.updateState);
+  }
+
+  async captureArea() {
+    console.log('emitting capture');
+    const user = this.props.navigation.getParam('user', false);
+    if (user) {
+      await this.socket.emit('capture', {
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        userId: user.id,
+      });
+    }
+  }
+
+  render() {
+    let latitude = this.state.latitude;
+    let longitude = this.state.longitude;
+    return (
+      <View style={StyleSheet.absoluteFillObject}>
+        <StatusBar hidden={false} />
+        {this.state.latitude ? (
+          <MapView
+            style={{ ...StyleSheet.absoluteFillObject, flex: 1 }}
+            initialRegion={{
+              latitude,
+              longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            onUserLocationChange={pos => {
+              this.setState({
+                latitude: pos.nativeEvent.coordinate.latitude,
+                longitude: pos.nativeEvent.coordinate.longitude,
+              });
+            }}
+            showsCompass={false}
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+          >
+            {Object.values(this.state.caps).map(cap => {
+              return (
+                <Circle
+                  key={cap.id}
+                  strokeWidth={1}
+                  fillColor={cap.user.team.color}
+                  radius={cap.radius}
+                  center={{
+                    latitude: cap.latitude,
+                    longitude: cap.longitude,
+                  }}
+                />
+              );
+            })}
+            <Circle
+              center={{
+                latitude,
+                longitude,
+              }}
+              strokeWidth={4}
+              strokeColor={'#00000030'}
+              radius={this.state.playerRadius}
+            />
+          </MapView>
+        ) : (
+          <View style={styles.container}>
+            <ActivityIndicator size="large" color="#0000ff" />
+            <Text>Determing your location...</Text>
+          </View>
+        )}
+        {this.state.latitude ? (
+          <View>
+            {this.props.navigation.getParam('user', false) ? (
+              <View style={styles.footer}>
+                <Button
+                  buttonStyle={{
+                    borderRadius: 35,
+                    width: 70,
+                    height: 70,
+                    flex: 1,
+                    justifyContent: 'center',
+                    backgroundColor: this.props.navigation.getParam('user', 0)
+                      .team.color,
+                  }}
+                  title="Capture"
+                  onPress={() => this.captureArea()}
+                />
+              </View>
+            ) : (
+              undefined
+            )}
+            {/* Menu Button */}
+            <View
+              style={{
+                top: sBarHeight + 20,
+                left: 20,
+                width: 40,
+                height: 40,
+              }}
+            >
+              <Icon
+                size={40}
+                name="menu"
+                color="#000000"
+                onPress={() => this.props.navigation.openDrawer()}
+              />
+            </View>
+          </View>
+        ) : (
+          <View />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footer: {
+    position: 'absolute',
+    top: Dimensions.get('window').height - 150,
+    width: Dimensions.get('window').width,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  navButton: {
+    position: 'absolute',
+    top: 50,
+    width: 50,
+  },
+});
